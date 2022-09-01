a34d63e1c21991564d4659b8be7e3913faeb0bef
William Moela <wmasehlele@gmail.com>
2022-09-01 18:20:33 +0200

    updated for mobile view port

--- src/components/app/NavigationComponent.vue
+++ src/components/app/NavigationComponent.vue
@@ -1,12 +1,18 @@
 <template>
     <nav class="bg-danger side-nav" id="side-nav">        
-        <div class="app-name d-flex flex-row align-items-start">
+        <!-- <div class="app-name d-flex flex-row align-items-start">
             <router-link class="navbar-brand" :to="{ name: 'dashboard' }"> 
                 <i class="bi bi-cloud-arrow-down-fill"></i>
                 Cloud Property
             </router-link>
-        </div>
+        </div> -->
         <ul class="navbar-nav">
+            <li class="nav-item app-name ">    
+                <router-link class="navbar-brand" :to="{ name: 'dashboard' }"> 
+                    <i class="bi bi-cloud-arrow-down-fill"></i>
+                    Cloud Property
+                </router-link>            
+            </li>            
             <li class="nav-item active">    
                 <router-link class="nav-link side-nav-link" :to="{ name: 'dashboard' }"> 
                     <i class="bi bi-speedometer2"></i>
